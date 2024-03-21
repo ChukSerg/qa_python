86c162eaa2e9546ca89b0916169655217f793750
Sergey Chukin <chuk2208@yandex.ru>
2024-03-21 22:41:43 +0500

    "Добавлены тесты для BooksCollector"

--- tests.py
+++ tests.py
@@ -1,24 +1,51 @@
+import pytest
+
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
+    @pytest.fixture(autouse=True)
+    def add_books(self):
+        self.collector = BooksCollector()
+        self.collector.add_new_book('Гордость и предубеждение и зомби')
+        self.collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+        assert len(self.collector.books_genre) == 2
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    def test_add_new_book_add_book_twice(self):
+        self.collector.add_new_book('Гордость и предубеждение и зомби')
+        assert len(self.collector.books_genre) == 2
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_add_new_book_book_has_not_genre(self):
+        assert self.collector.get_book_genre('Гордость и предубеждение и зомби') == ''
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_set_book_genre_add_genre(self):
+        self.collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
+        assert self.collector.get_book_genre('Гордость и предубеждение и зомби') == 'Ужасы'
+
+    def test_get_books_for_children_return_book_age_rating(self):
+        self.collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
+        self.collector.set_book_genre('Что делать, если ваш кот хочет вас убить', 'Комедии')
+        assert self.collector.get_books_for_children() == ['Что делать, если ваш кот хочет вас убить']
+
+    def test_get_books_with_specific_genre_return_book_genre(self):
+        self.collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
+        self.collector.set_book_genre('Что делать, если ваш кот хочет вас убить', 'Комедии')
+        assert self.collector.get_books_with_specific_genre('Комедии') == ['Что делать, если ваш кот хочет вас убить']
+
+    def test_add_book_in_favorites_add_one_book(self):
+        self.collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+        self.collector.add_book_in_favorites('Незнайка на луне')
+        assert len(self.collector.get_list_of_favorites_books()) == 1
+
+    def test_delete_book_from_favorites_stay_one_book(self):
+        self.collector.add_book_in_favorites('Гордость и предубеждение и зомби')
+        self.collector.add_book_in_favorites('Что делать, если ваш кот хочет вас убить')
+        self.collector.delete_book_from_favorites('Что делать, если ваш кот хочет вас убить')
+        assert len(self.collector.get_list_of_favorites_books()) == 1
+
+    def test_get_books_genre_return_two_books(self):
+        assert len(self.collector.get_books_genre()) == 2
